6874dfea7388f96f56ccf3ff849331b0404206da
Thom Rey <mochi023@users.noreply.github.com>
2021-01-30 18:31:20 +0800

    Added frontend components

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateComponent } from './components/create/create.component';
+import { DeleteComponent } from './components/delete/delete.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    DashboardComponent,
+    CreateComponent,
+    DeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
