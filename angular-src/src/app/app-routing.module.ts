039d15811d2803a4a3a0d116f22ad0e9e9980390
Thom Rey <mochi023@users.noreply.github.com>
2021-01-31 18:35:52 +0800

    components
    made the components plus the login frontend

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'create', component: CreateComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'profile', component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateComponent } from './components/create/create.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+// import { ValidateService } from './services/validate.service';
+// import { FlashMessagesModule } from 'flash-messages-angular';
+// import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    CreateComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    // FlashMessagesModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    // ValidateService,
+    // AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
